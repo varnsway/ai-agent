b8fe3a7b86df81f1c012c43bef4c17ea70d6e2db
sway-sea <itraynes@gmail.com>
2025-07-04 11:01:38 +0200

    second commit

---
+++ functions/get_files_info.py
@@ -0,0 +1,1 @@
+def get_files_info(working_directory, directory=None):
--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 # Load environment variables from .env file
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-model = "gemini-2.0-flash-001"
-contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
-result = client.models.generate_content(model=model, contents=contents)
+messages = [
+    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+]
+
+model = "gemini-2.0-flash-001"
+
+
+result = client.models.generate_content(model=model, contents=messages)
 prompt_tokens = result.usage_metadata.prompt_token_count
 response_tokens = result.usage_metadata.candidates_token_count
 
 
 def main():
+    if len(sys.argv) > 2:
+        print("Error: invalid number of arguments.")
+        sys.exit(1)
     print(result.text)
     print(f"Prompt tokens: {prompt_tokens}")
     print(f"Response tokens: {response_tokens}")
